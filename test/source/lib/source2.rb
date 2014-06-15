6a725449700dc07a19cc1f85b691cb3e76292181
Jens Nazarenus <jens@0x6a.de>
2014-06-15 11:42:19 +0200

    Add another test source

---
+++ test/source/lib/source2.rb
@@ -0,0 +1,18 @@
+module TesModule
+
+  class TestClass2
+
+    # This initializes the test class2
+    #
+    # options - the passed options as hash
+    def initialize
+    end
+
+    # This method does something really big
+    # Returns a hash with some other things
+    def method_a
+    end
+
+  end
+
+end
